c1a091cb1d4c8b21e53c642ed93b7d3c215d78cb
codeping <deping.du@gmail.com>
2020-06-17 22:27:37 +0800

    项目初始化。

---
+++ src/ConsulExtensions/ConsulServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Consul;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ConsulServiceCollectionExtensions
+    {
+        public static IServiceCollection AddConsulClient(this IServiceCollection services)
+        {
+            return services.AddTransient<IConsulClient, ConsulClient>();
+        }
+
+        public static IServiceCollection AddConsulClient(
+            this IServiceCollection services, Action<ConsulClientConfiguration> configOptions)
+        {
+            return services.AddTransient<IConsulClient>(provider =>
+            {
+                return new ConsulClient(configOptions);
+            });
+        }
+
+        public static IServiceCollection AddConsulClient(
+            this IServiceCollection services, Action<IServiceProvider, ConsulClientConfiguration> configOptions)
+        {
+            return services.AddTransient<IConsulClient>(provider =>
+            {
+                return new ConsulClient(options => configOptions?.Invoke(provider, options));
+            });
+        }
+    }
+}
